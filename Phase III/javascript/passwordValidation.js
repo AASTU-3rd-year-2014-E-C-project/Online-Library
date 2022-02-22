5cbe22701ac0309181ddd6d804c5aab1d0674602
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2022-02-22 07:38:22 +0300

    update

--- Phase III/javascript/passwordValidation.js
+++ Phase III/javascript/passwordValidation.js
@@ -89,3 +89,96 @@ confirmPassword.onkeyup = function() {
     document.getElementById("confirmPsw").style.border = "1px solid red";
   }
 }
+//validation2
+
+var newPassword2 = document.getElementById("newPsw2");
+var confirmPassword2 = document.getElementById("confirmPsw2");
+var changeBorder2 = document.getElementById("pswBorder2");
+var letter2 = document.getElementById("letter2");
+var capital2 = document.getElementById("capital2");
+var number2 = document.getElementById("number2");
+var length2 = document.getElementById("length2");
+
+
+newPassword2.onfocus = function() {
+    //don't display if all condition fulfill
+    if (letter2.className === 'valid2'&&number2.className === 'valid2'&&capital2.className === 'valid2'&&length2.className === 'valid2')
+       document.getElementById("message2").style.display = "none";
+   else
+       document.getElementById("message2").style.display = "block";
+
+}
+newPassword2.onblur = function() {
+  document.getElementById("message2").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+newPassword2.onkeyup = function() {
+
+
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(newPassword2.value.match(lowerCaseLetters)) {  
+    letter2.classList.remove("invalid2");
+    letter2.classList.add("valid2");
+  } else {
+    letter2.classList.remove("valid2");
+    letter2.classList.add("invalid2");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(newPassword2.value.match(upperCaseLetters)) {  
+    capital2.classList.remove("invalid2");
+    capital2.classList.add("valid2");
+  } else {
+    capital2.classList.remove("valid2");
+    capital2.classList.add("invalid2");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(newPassword2.value.match(numbers)) {  
+    number2.classList.remove("invalid2");
+    number2.classList.add("valid2");
+  } else {
+    number2.classList.remove("valid2");
+    number2.classList.add("invalid2");
+  }
+  
+  // Validate length
+  if(newPassword2.value.length >= 8) {
+    length2.classList.remove("invalid2");
+    length2.classList.add("valid2");
+  } else {
+    length2.classList.remove("valid2");
+    length2.classList.add("invalid2");
+  }
+
+  //don't display if all condition fulfill
+  if (letter2.className === 'valid2'&&number2.className === 'valid2'&&capital2.className === 'valid2'&&length2.className === 'valid2')
+  document.getElementById("message2").style.display = "none";
+
+}
+
+//check password confirmation
+confirmPassword2.onfocus = function() {
+ 
+    changeBorder2.classList.remove("pswStyle_onblur2");
+    changeBorder2.classList.add("pswStyle_focus2");
+  
+}
+confirmPassword2.onblur = function() {
+  changeBorder2.classList.remove("pswStyle_focus2");
+  changeBorder2.classList.add("pswStyle_onblu2");
+  
+}
+
+confirmPassword2.onkeyup = function() {
+  if(confirmPassword2.value.match(newPassword2.value)){
+      document.getElementById("confirmPsw2").style.border = "3px solid green";
+  }
+  else{
+    document.getElementById("confirmPsw2").style.border = "3px solid red";
+  }
+}
