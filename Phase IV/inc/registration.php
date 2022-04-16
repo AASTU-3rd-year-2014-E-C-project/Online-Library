3bf1ab498ca225f0f87e9d1fb617c62e6670616c
nahom-getachew7gi5 <ets050012feng@aastu.edu.et>
2022-04-16 11:21:04 +0300

    database and form connection with database

--- Phase IV/inc/registration.php
+++ Phase IV/inc/registration.php
@@ -1,10 +1,10 @@
 <?php
 
     $host = "localhost";
-    $username = "root";
-    $password = "your_password";
-    $db_name = "aastu_library";
-    $table_name = "login";
+    $host_username = "root";
+    $host_password = "";
+    $db_name = "aastu-library";
+    $table_name = "user";
 
     // user registration input
     $fName = $_POST['fname'];
@@ -15,10 +15,11 @@
     $phone = $_POST['phone'];
     $password = $_POST['newPassword'];
 
-    $conn = mysqli_connect($host, $username, $password, $db_name);
+    $conn = mysqli_connect($host, $host_username, $host_password, $db_name);
 
     $query = "INSERT INTO $table_name(first_name, last_name, gender, username, password, email, phone) VALUES ('$fName', '$lName', '$gender', '$username', '$password', '$email', '$phone')";
 
     mysqli_query($conn, $query);
+    header("Location: ../index.html");
 
 ?>
