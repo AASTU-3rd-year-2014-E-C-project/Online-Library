83c5ae34b71f5cda1446e636c79fdbae1f6bd62f
nahom-getachew7gi5 <ets050012feng@aastu.edu.et>
2022-04-16 09:20:22 +0300

    registration

---
+++ Phase IV/inc/registration.php
@@ -0,0 +1,24 @@
+<?php
+
+    $host = "localhost";
+    $username = "root";
+    $password = "your_password";
+    $db_name = "aastu_library";
+    $table_name = "login";
+
+    // user registration input
+    $fName = $_POST['fname'];
+    $lName = $_POST['lname'];
+    $username = $_POST['Username'];
+    $gender = $_POST['radSize'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['newPassword'];
+
+    $conn = mysqli_connect($host, $username, $password, $db_name);
+
+    $query = "INSERT INTO $table_name(first_name, last_name, gender, username, password, email, phone) VALUES ('$fName', '$lName', '$gender', '$username', '$password', '$email', '$phone')";
+
+    mysqli_query($conn, $query);
+
+?>
