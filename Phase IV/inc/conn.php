75db149353eaffb3003387fd6b983f59af7b28f2
Nahom Habtamu <nhabtamu42@gmail.com>
2022-04-18 23:52:40 +0300

    added read online count function

--- Phase IV/inc/conn.php
+++ Phase IV/inc/conn.php
@@ -6,4 +6,6 @@ $host_password = "";
 $db_name = "aastu_library";
 $table_name = "user";
 
+$conn = mysqli_connect($host, $host_username, $host_password, $db_name);
+
 ?>
--- Phase IV/inc/login.php
+++ Phase IV/inc/login.php
@@ -4,8 +4,6 @@
      $username = $_POST['logName'];
      $password = $_POST['logPassword'];
 
-      $conn = mysqli_connect($host, $host_username, $host_password, $db_name);
-
       $login= "SELECT password FROM $table_name WHERE username='$username'";
         $result= mysqli_query($conn, $login);
 
