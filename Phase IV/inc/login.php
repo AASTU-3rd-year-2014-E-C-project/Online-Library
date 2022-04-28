b64e62f72cc4e57082c5035a8a499b736610637e
Nahom Habtamu <nhabtamu42@gmail.com>
2022-04-28 11:51:37 +0300

    added admin login

--- Phase IV/inc/login.php
+++ Phase IV/inc/login.php
@@ -1,19 +1,33 @@
 <?php
-    include_once("conn.php");
-    include_once("session.php");
+include_once("conn.php");
+include_once("session.php");
 
-     $username = $_POST['logName'];
-     $password = $_POST['logPassword'];
+$username = $_POST['logName'];
+$password = $_POST['logPassword'];
 
-      $login= "SELECT password, user_id FROM $table_name WHERE username='$username'";
-        $result= mysqli_query($conn, $login);
+$login = "SELECT password, user_id FROM $table_name WHERE username='$username'";
+$result = mysqli_query($conn, $login);
 
-        while($row = mysqli_fetch_assoc($result)){
-           
-            if ($row['password'] == $password){
-                $_SESSION['user_id'] = $row['user_id'];
-                header('Location: ../public/book_list.php');
-                die();
-            } 
-        }
-               header("Location: ../index.php?error=invalid");
+while ($row = mysqli_fetch_assoc($result)) {
+
+    if ($row['password'] == $password) {
+        $_SESSION['user_id'] = $row['user_id'];
+        $_SESSION['user_type'] = 'user';
+        header('Location: ../public/book_list.php');
+        die();
+    }
+}
+
+$login = "SELECT password, admin_id FROM admin WHERE username='$username'";
+$result = mysqli_query($conn, $login);
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    if ($row['password'] == $password) {
+        $_SESSION['user_id'] = $row['admin_id'];
+        $_SESSION['user_type'] = 'admin';
+        header('Location: ../public/report.php');
+        die();
+    }
+}
+header("Location: ../index.php?error=invalid");
