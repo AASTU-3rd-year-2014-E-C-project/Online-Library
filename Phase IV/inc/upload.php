3706beb86785e84357b9ef628f28b03cff3e8aca
nahom-getachew7gi5 <ets050012feng@aastu.edu.et>
2022-05-08 14:08:42 +0300

    connecting upload page to database

--- Phase IV/inc/upload.php
+++ Phase IV/inc/upload.php
@@ -1,4 +1,6 @@
 <?php
+include_once("conn.php");
+include_once("session.php");
 if (isset($_POST['submit'])){
     
     $fileName = $_FILES['myfile']['name'];
@@ -59,6 +61,19 @@ if (isset($_POST['submit'])){
         echo "The file format of cover is only jpg,jpeg and png for the moment";
     }
 }
+$table = 'resource';
+$type = $_POST['radio'];
+$title = $_POST['Title'];
+$author = $_POST['Author'];
+$desc = $_POST['description'];
+$user = $_SESSION['user_id'];
+//$cover = $_POST['cover'];  cover file name is $fileNameNewI
+// $file = $_POST['myfile'];     the name of the file is $fileNameNew
+
+$query = "INSERT INTO $table(resource_type, resource_title, resource_author, resource_desc, resource_cover, resource_file, user_id) VALUES ('$type', '$title', '$author', '$desc', '$fileNameNewI', '$fileNameNew', ' $user')";
+
+    mysqli_query($conn, $query);
+    header("Location: ../public/upload.php");
 
 
 ?>
