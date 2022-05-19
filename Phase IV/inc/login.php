dc59ad8b05540f0e33cc21e23bd0d749e72ccc47
Nahom Habtamu <nhabtamu42@gmail.com>
2022-05-19 15:49:10 +0300

    login update (user and admin radio)

--- Phase IV/inc/login.php
+++ Phase IV/inc/login.php
@@ -5,29 +5,41 @@ include_once("session.php");
 $username = $_POST['logName'];
 $password = $_POST['logPassword'];
 
-$login = "SELECT password, user_id FROM $table_name WHERE username='$username'";
-$result = mysqli_query($conn, $login);
+if ($_POST['user-type'] == 'user') {
+    $login = "SELECT password, user_id FROM $table_name WHERE username='$username'";
+    $result = mysqli_query($conn, $login);
 
-while ($row = mysqli_fetch_assoc($result)) {
+    while ($row = mysqli_fetch_assoc($result)) {
 
-    if (password_verify($password, $row['password'])) {
-        $_SESSION['user_id'] = $row['user_id'];
-        $_SESSION['user_type'] = 'user';
-        header('Location: ../public/book_list.php');
-        die();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['user_id'];
+            $_SESSION['user_type'] = 'user';
+            header('Location: ../public/book_list.php');
+            die();
+        }else {
+            header("Location: ../index.php?error=invalid");
+        }
+        header("Location: ../index.php?error=invalid");
     }
+} else if ($_POST['user-type'] == 'admin') {
+    
+    $login = "SELECT password, admin_id FROM admin WHERE username='$username'";
+    $result = mysqli_query($conn, $login);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['admin_id'];
+            $_SESSION['user_type'] = 'admin';
+            header('Location: ../public/report.php');
+            die();
+        }else {
+            header("Location: ../index.php?error=invalid");
+        }
+    }
+    header("Location: ../index.php?error=invalid");
+} else {
+    header("Location: ../index.php?error=invalid");
 }
 
-$login = "SELECT password, admin_id FROM admin WHERE username='$username'";
-$result = mysqli_query($conn, $login);
-
-while ($row = mysqli_fetch_assoc($result)) {
-
-    if (password_verify($password, $row['password'])) {
-        $_SESSION['user_id'] = $row['admin_id'];
-        $_SESSION['user_type'] = 'admin';
-        header('Location: ../public/report.php');
-        die();
-    }
-}
-header("Location: ../index.php?error=invalid");
+?>
