0864a99b35f94e24a53e671357fabd0710008e93
Nahom Habtamu <nhabtamu42@gmail.com>
2022-06-08 22:03:22 +0300

    update

--- Phase IV/public/book1.php
+++ Phase IV/public/book1.php
@@ -25,5 +25,27 @@ if (isset($_SESSION['user_id'])) {
 
     // Send the file to the browser.
     readfile($filename);
+
+//     header("Content-Length: " . filesize ("../uploads/resource_files/' . {$row['resource_file']}" ) ); 
+// header("Content-type: application/pdf"); 
+// header("Content-disposition: inline;     
+// filename=".basename("../uploads/resource_files/' . {$row['resource_file']}"));
+// header('Expires: 0');
+// header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+// $filepath = readfile("../uploads/resource_files/' . {$row['resource_file']}");
+?>
+    <!-- <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Read Online</title>
+    </head>
+    <body>
+        <iframe src="../uploads/resource_files/" frameborder="0"></iframe>
+    </body>
+    </html> -->
+<?php
 } else
-    header("Location: ../index.php");
+    header("Location: ../index.php"); ?>
