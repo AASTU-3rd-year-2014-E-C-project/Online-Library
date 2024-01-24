7472b1a1550c7c7e5d002cc8024a849a3cc4247b
Nahom Habtamu <nhabtamu42@gmail.com>
2024-01-24 23:31:00 +0300

    SCM remove ratings and comments

--- Phase IV/public/book1_desc.php
+++ Phase IV/public/book1_desc.php
@@ -165,253 +165,70 @@ if (isset($_SESSION['user_id'])) {
                             ?>
                         </h4>
 
-                        <?php
+                        <style>
+                            .rating-label {
+                                font-weight: bold;
+                            }
 
-                        $q_rating = "SELECT * FROM rating WHERE resource_id=$resource_id AND user_id={$_SESSION['user_id']}";
-                        $rating_res = mysqli_query($conn, $q_rating);
-                        $rating_no = 0;
-                        if (mysqli_num_rows($rating_res) > 0) {
-                            $rating_no = mysqli_fetch_assoc($rating_res)['rating'];
-                        }
+                            /* Rating bar width */
+                            .rating-bar {
+                                width: 300px;
+                                padding: 8px;
+                                border-radius: 5px;
+                            }
 
-                        ?>
+                            /* The bar container */
+                            .bar-container {
+                                width: 100%;
+                                background-color: #f1f1f1;
+                                text-align: center;
+                                color: white;
+                                border-radius: 20px;
+                                cursor: pointer;
+                                margin-bottom: 5px;
+                            }
 
-                        <form action="../inc/rating.php?resource_id=<?= $resource_id ?>" method="POST">
-                            <div class="rating-container">
-                                <div class="star-container" style="margin-top: 15px;">
-                                    <div class="stars">
-                                        <input type="radio" name="rate" id="rate-5" value="5" <?php echo $rating_no == 5 ? 'checked' : '' ?>>
-                                        <label for="rate-5" class="fa fa-star"></label>
-                                        <input type="radio" name="rate" id="rate-4" value="4" <?php echo $rating_no == 4 ? 'checked' : '' ?>>
-                                        <label for="rate-4" class="fa fa-star"></label>
-                                        <input type="radio" name="rate" id="rate-3" value="3" <?php echo $rating_no == 3 ? 'checked' : '' ?>>
-                                        <label for="rate-3" class="fa fa-star"></label>
-                                        <input type="radio" name="rate" id="rate-2" value="2" <?php echo $rating_no == 2 ? 'checked' : '' ?>>
-                                        <label for="rate-2" class="fa fa-star"></label>
-                                        <input type="radio" name="rate" id="rate-1" value="1" <?php echo $rating_no == 1 ? 'checked' : '' ?>>
-                                        <label for="rate-1" class="fa fa-star"></label>
-                                    </div>
-                                </div>
-                                <input type="submit" name="rate-btn" class="rate-btn" value="Rate">
-                            </div>
-                            <!-- rating progress -->
+                            /* Individual bars */
+                            .bar-5 {
+                                width: 70%;
+                                height: 13px;
+                                background-color: #FBC02D;
+                                border-radius: 20px;
 
-                            <div class="container-fluid px-1 py-5 mx-auto">
+                            }
 
-                                <div class="col-md-8">
-                                    <div class="rating-bar0 justify-content-center">
-                                        <table class="text-left mx-auto">
-                                            <tr>
-                                                <td class="rating-label">5 - Excellent</td>
-                                                <td class="rating-bar">
-                                                    <div class="bar-container">
-                                                        <div class="bar-5" style="width: 0%" ;"></div>
-                                                    </div>
-                                                </td>
-                                                <td class="text-right">(<span class="count-rate-5">0</span>)</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="rating-label">4 - Good</td>
-                                                <td class="rating-bar">
-                                                    <div class="bar-container">
-                                                        <div class="bar-4" style="width: 0%" ;"></div>
-                                                    </div>
-                                                </td>
-                                                <td class="text-right">(<span class="count-rate-4">0</span>)</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="rating-label">3 - Average</td>
-                                                <td class="rating-bar">
-                                                    <div class="bar-container">
-                                                        <div class="bar-3" style="width: 0%" ;"></div>
-                                                    </div>
-                                                </td>
-                                                <td class="text-right">(<span class="count-rate-3">0</span>)</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="rating-label">2 - Poor</td>
-                                                <td class="rating-bar">
-                                                    <div class="bar-container">
-                                                        <div class="bar-2" style="width: 0%" ;"></div>
-                                                    </div>
-                                                </td>
-                                                <td class="text-right">(<span class="count-rate-2">0</span>)</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="rating-label">1 - Terrible</td>
-                                                <td class="rating-bar">
-                                                    <div class="bar-container">
-                                                        <div class="bar-1" style="width: 0%" ;"></div>
-                                                    </div>
-                                                </td>
-                                                <td class="text-right">(<span class="count-rate-1">0</span>)</td>
-                                            </tr>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                    </div>
+                            .bar-4 {
+                                width: 30%;
+                                height: 13px;
+                                background-color: #FBC02D;
+                                border-radius: 20px;
 
-                    <style>
-                        .rating-label {
-                            font-weight: bold;
-                        }
+                            }
 
-                        /* Rating bar width */
-                        .rating-bar {
-                            width: 300px;
-                            padding: 8px;
-                            border-radius: 5px;
-                        }
+                            .bar-3 {
+                                width: 20%;
+                                height: 13px;
+                                background-color: #FBC02D;
+                                border-radius: 20px;
 
-                        /* The bar container */
-                        .bar-container {
-                            width: 100%;
-                            background-color: #f1f1f1;
-                            text-align: center;
-                            color: white;
-                            border-radius: 20px;
-                            cursor: pointer;
-                            margin-bottom: 5px;
-                        }
+                            }
 
-                        /* Individual bars */
-                        .bar-5 {
-                            width: 70%;
-                            height: 13px;
-                            background-color: #FBC02D;
-                            border-radius: 20px;
+                            .bar-2 {
+                                width: 10%;
+                                height: 13px;
+                                background-color: #FBC02D;
+                                border-radius: 20px;
 
-                        }
+                            }
 
-                        .bar-4 {
-                            width: 30%;
-                            height: 13px;
-                            background-color: #FBC02D;
-                            border-radius: 20px;
+                            .bar-1 {
+                                width: 0%;
+                                height: 13px;
+                                background-color: #FBC02D;
+                                border-radius: 20px;
 
-                        }
-
-                        .bar-3 {
-                            width: 20%;
-                            height: 13px;
-                            background-color: #FBC02D;
-                            border-radius: 20px;
-
-                        }
-
-                        .bar-2 {
-                            width: 10%;
-                            height: 13px;
-                            background-color: #FBC02D;
-                            border-radius: 20px;
-
-                        }
-
-                        .bar-1 {
-                            width: 0%;
-                            height: 13px;
-                            background-color: #FBC02D;
-                            border-radius: 20px;
-
-                        }
-                    </style>
-
-                    </form>
-                    <!-- rating progress end -->
-
-                    <div class="buttons">
-                        <form method="POST">
-                            <!-- <a href="book1.html" class="read-btn">Read Online</a> -->
-                            <input type="submit" name="read-online-btn" class="read-btn" value="Read Online" style="text-decoration: none;
-                color: #000;
-                border: 1px solid rgb(0, 157, 219);
-                background-color: rgb(0, 157, 219);
-                font-size: 19px;
-                border-radius: 7px;
-                cursor: pointer;
-                padding: 7px;
-                font-weight: bold;
-                margin-left: 8px;
-                color: #fff;">
-                            <!-- <a href="resources/Books/the_philosophy_of_history.pdf " class="download-btn" Download>Download</a> -->
-                            <input type="submit" name="download-btn" class="download-btn" value="Download" style="text-decoration: none;
-                color: #000;
-                border: 1px solid rgb(0, 157, 219);
-                background-color: rgb(0, 157, 219);
-                font-size: 19px;
-                border-radius: 7px;
-                cursor: pointer;
-                padding: 7px;
-                font-weight: bold;
-                margin-left: 8px;
-                color: #fff;">
-                        </form>
-                    </div>
-                </div>
-                <div class="other comments-container">
-                    <h2>Comments</h2>
-                    <hr>
-                    <div class="comments">
-
-                        <?php
-
-                        $comment_query = "SELECT * FROM comment WHERE resource_id=$resource_id";
-                        $comment_result = mysqli_query($conn, $comment_query);
-
-                        while ($comm_info = mysqli_fetch_assoc($comment_result)) {
-
-                        ?>
-
-
-                            <div class="comment">
-                                <div class="profile-pic-container">
-                                    <img src="../image/login_icon.jpg" alt="" class="profile-pic">
-                                </div>
-                                <div class="name-date">
-                                    <?php
-                                    $commenter_name_query = "SELECT first_name, last_name FROM user WHERE user_id={$comm_info['user_id']}";
-                                    $commenter_name_result = mysqli_query($conn, $commenter_name_query);
-                                    $commenter = mysqli_fetch_assoc($commenter_name_result);
-
-                                    ?>
-                                    <h3 class="comment-giver-name"><?= $commenter['first_name'] . ' ' . $commenter['last_name'] ?></h3>
-                                    <h4 class="date-and-time"><?= $comm_info['comment_date'] ?></h4>
-                                </div>
-                                <p class="comment-text"><?= $comm_info['comment'] ?></p>
-                            </div>
-
-                        <?php } ?>
-
-
-                    </div>
-                    <button id="addCommentBtn" onclick="commentPopup()">Add Comment</button>
-                </div>
-                <!-- <div class="other related-container">
-                        <h2>Related books</h2>
-                        <hr>
-                        <div class="book-container related-books">
-                            jj
-                        </div>
-                    </div> -->
-            </div>
-
-            <form action="comment.inc.php" method="POST">
-                <div class="add-comment-form">
-                    <h3 class="comment-add-title">Comment</h3>
-
-                    <textarea name="comment" id="" cols="40" rows="10" class="comment-field comment-text-area"></textarea>
-
-                    <div class="comment-add-btn">
-                        <button type="submit" id="addCommBtn" class="comment-btn add-comment-btn" name="commentSubmit">Comment</button>
-                        <button type="button" id="cancelCommBtn" class="comment-btn cancel-btn">Cancel</button>
-                    </div>
-                </div>
-                <script src="../javascript/jquery-3.6.0.min.js"></script>
-                <script src="../javascript/book_desc.js"></script>
-                <script src="../javascript/dropdown.js"></script>
-                <script src="../javascript/book_list.js"></script>
-                </div>
+                            }
+                        </style>
         </body>
         </form>
 
